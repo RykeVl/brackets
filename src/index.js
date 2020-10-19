6b0e61b27ad37b8e2979d74a598aa07bc4199b09
RykeVl <iforox@gmail.com>
2020-10-19 22:51:18 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+    let string = [];
+    let brackets = Object.fromEntries(bracketsConfig);
+
+    for (let bracket of str.split('')) {
+        if (string[0] === bracket) {
+            string.shift();
+        } else if (brackets[bracket]) {
+            string.unshift(brackets[bracket]);
+        } else {
+            return false;
+        }
+    }
+
+    return string.length === 0;
 }
